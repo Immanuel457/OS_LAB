2ecf408fc48ba7fd80d3d523ffcff7fd083d7040
Immanuel457 <immanuelyoshua457@gmail.com>
2024-05-18 20:06:14 +0530

    Commiting pages.c before testing for error

--- page.c
+++ page.c
@@ -4,12 +4,54 @@
 //___________________________________________________________________________
 typedef struct paging_stack{
     int id;
+    int count;
+    int index;
 }stack;
 int FRONT = -1;
-void push(){
-
-}
 //____________________________________________________________________________
+void check_similar(int temp,int n_pf,int s[n_pf]){
+    int flag = 0;
+    for(int i = 0;i < n_pf;i++){
+        if(temp == s[i]){
+            flag = 1;
+        }
+    }
+    return flag;
+}
+void display(int n_pf,int s[n_pf]){
+    int i;
+    for(i = 0;i < n_pf;i++){
+        printf("%d,",s[i]);
+    }
+}
+void FCFS(int n_p,int n_pf,int p[n_p],int s[n_pf]){
+    int i,j,hit,check;
+    for(i = 0;i < n_p;i++){
+        while(j < n_pf){
+            if(s[j] == -1){
+                s[j] = p[i];
+                break;
+            }
+            else{
+                check = check_similar(p[i],n_pf,s);
+                if(check != 1){
+                    check = hit;
+                    s[j] = p[i];
+                    j++;
+                    break;
+                }
+                else{
+                    hit++;
+                    j++;
+                    break;
+                }
+            }
+        }
+        if(j == n_p) j = 0;
+
+        display(n_pf,s);
+    }
+}
 void initialize_page_frame(int n_p,int p[n_p]){
     int n_pf;
     printf("Enter the number of frames in a single page : ");
@@ -17,7 +59,9 @@ void initialize_page_frame(int n_p,int p[n_p]){
 
     // initializing the page frame 
     stack s[n_pf];
-
+    for(int i = 0;i < n_pf;i++){
+        s[i] = -1;
+    }
     int choice;
     printf("Enter your choice : \n");
     printf("1.FCFS");
