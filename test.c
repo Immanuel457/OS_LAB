66dcacd31958b40073eb96efc294a9f0ef978741
Immanuel457 <immanuelyoshua457@gmail.com>
2024-05-22 19:55:25 +0530

    comit  before testing

--- test.c
+++ test.c
@@ -1,114 +1,106 @@
 #include<stdio.h>
-#include<sys/types.h>
-#include<sys/unistd.h>
-#include<semaphores.h>
-#include<wait.h>
-#include<signal.h>
+#include<stdlib.h>
+#define max 20
 
+typedef struct page_frame{
+    int id;
+    int index;
+}frame;
+int n_p;
+int n_pf = 4;
+int p[max];
+frame f[4];
+
+void display(){
+    for(int i = 0;i < n_pf;i++){
+        printf("%d",f[i]);
+    }
+}
+int check_similar(int i){
+    for(int j = 0;j < n_pf;j++){
+        if(p[i] == f[j].id){
+            return 1;
+        }
+    }
+    return 0;
+}
+void future_index(int i){
+    int j,k = 0,index = 0;
+    for(j = i+1;j < n_p;j++){
+        while(k < n_pf){
+            if(f[k].id == p[j]){
+                f[k].index = index;
+                index++;
+            }
+            k++;
+        }
+    }
+}
+void Optimal(){
+    int i,j = 0,hit,check;
+    for(i = 0;i < n_p;i++){
+        while(j < n_pf){
+            if(f[j].id == -1){
+                future_index(i);
+                check = check_similar(i);
+                if(check != 1){
+                    f[j].id = p[i];
+                    j++;
+                    break;
+                }
+                else{
+                    hit++;
+                    j++;
+                    break;
+                }
+            }
+            else{
+                future_index(i);
+                check = check_similar(i);
+                if(check != 1){
+                    int k = 0;
+                    int min = 0;
+                    while(k < n_pf){
+                        if(min > f[k].index){
+                            min = f[k].index;
+                            j = k;
+                        }
+                        k++;
+                    }
+                    f[j].id = p[i];
+                    j++;
+                    break;
+                }
+                else{
+                    hit++;
+                    j++;
+                    break;
+                }
+            }
+        }
+        display();
+        printf("\n");
+    }
+}
+void initialize_frame(){
+    for(int i = 0;i < n_pf;i++){
+        f[i].id = -1;
+        f[i].index = 0;
+    }
+}
+void initialize(){
+    //printf("Enter the number of process : ");
+    //scanf("%d",&n_p);
+    n_p = 14;
+    p[] = {1,2,3,4,3,1,2,4,5,2,1,2,3,4};
+    //printf("Enter the process id  : \n");
+    /*for(int i = 0;i < n;i++){
+        scanf("%d",&p[i]);
+    }*/
+    initialize_frame();
+}
 int main(){
-    pid_t p0 = fork();
-    printf("Currently in process p0 pid = %d\n",getpid());
-    printf("Creating child process p1.\n");
-    pid_t p1 = fork();
-    if(p1 == -1){
-        perror("fork p1 failed.\n");
-        exit(1);
-    }
-    else if(p1 == 0){
-        printf("Currently in process p1 pid = %d\n",getpid());
-        printf("Creating child process p2.\n");
-        pid_t p2 = fork();
-        if(p2 == -1){
-            perror("fork p2 failed.\n");
-            exit(1);
-        }
-        else if(p2 == 0){
-            printf("Currently in process p2 pid = %d\n",getpid());
-            printf("Creating child process p4.\n");
-            pid_t p4 = fork();
-            if(p4 == -1){
-                perror("fork p4 failed.\n");
-                exit(1);
-            }
-            else if(p4 == 0){
-                printf("Currently in process p4 pid = %d\n",getpid());
-                sleep(3);
-            }
-            else{
-                p4 = wait(NULL);
-                kill(p4,SIGTERM);
-                printf("process p4 terminated !!.\n");
-            }
-            printf("Creating child process p5.\n");
-            pid_t p5 = fork();
-            if(p5 == -1){
-                perror("fork p5 failed.\n");
-                exit(1);
-            }
-            else if(p5 == 0){
-                printf("Currently in process p5 pid = %d\n",getpid());
-                sleep(3);
-            }
-            else{
-                p5 = wait(NULL);
-                kill(p5,SIGTERM);
-                printf("process p5 terminated !!.\n");
-            }
-        }
-        else{
-            p2 = wait(NULL);
-            kill(p2,SIGTERM);
-            printf("process p2 terminated !!.\n");
-        }
-        printf("Creating child process p3.\n");
-        pid_t p3 = fork();
-        if(p3 == -1){
-            perror("fork p3 failed.\n");
-            exit(1);
-        }
-        else if(p3 == 0){
-            printf("Currently in process p3 pid = %d\n",getpid());
-            printf("Creating child process p6.\n");
-            pid_t p6 = fork();
-            if(p6 == -1){
-                perror("fork p6 failed.\n");
-                exit(1);
-            }
-            else if(p6 == 0){
-                printf("Currently in process p6 pid = %d\n",getpid);
-                sleep(3);
-            }
-            else{
-                p6 = wait(NULL);
-                kill("p6",SIGTERM);
-                printf("process p6 terminated !!.\n");
-            }
-            printf("Creating child process p7.\n");
-            pid_t p7 = fork();
-            if(p7 == -1){
-                perror("fork p7 failed.\n");
-                exit(1);
-            }
-            else if(p7 == 0){
-                printf("Currently in process p7 pid = %d\n",getpid);
-                sleep(3);
-            }
-            else{
-                p7 = wait(NULL);
-                kill("p7",SIGTERM);
-                printf("procewss p7 terminated !!.\n");
-            }
-        }
-        else{
-            p3 = wait(NULL);
-            kill(p3,SIGTERM);
-            printf("process p3 terminated !! .\n");
-        }
-    }
-    else{
-        p1 = wait(NULL);
-        kill(p1,SIGTERM);
-        printf("process p1 terminated !!.");
-    }
+    initialize();
+    Optimal();
     return 0;
 }
